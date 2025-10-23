f8c6452774e86d81a94eb12d3ad66314a11a4b3b
Gulistane Raza <gulistaneraza01@gmail.com>
2025-10-23 15:32:26 +0530

    core DNS

--- sqs/src/utils/constaints.js
+++
@@ -1,9 +0,0 @@
-import dot from "dotenv";
-dot.config();
-
-const region = process.env.REGION;
-const accessKeyId = process.env.ACCESSKEYID;
-const secretAccessKey = process.env.SECRETACCESSKEY;
-const queueUrl = process.env.QUEUEURL;
-
-export { region, accessKeyId, secretAccessKey, queueUrl };
